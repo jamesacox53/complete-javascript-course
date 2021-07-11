c7d682a35fffdf574b221e08aeb2ce958280c013
jamesacox53 <jamesacox53@gmail.com>
2021-07-11 12:10:42 +0100

    Completed Values and Variables.

---
+++ 01-Fundamentals-Part-1/myWork/assignments.js
@@ -0,0 +1,7 @@
+let country = "United Kingdom";
+let continent = 'Europe';
+let population = 66650000;
+
+console.log("country: " + country);
+console.log("continent: " + continent);
+console.log("population: " + population);
