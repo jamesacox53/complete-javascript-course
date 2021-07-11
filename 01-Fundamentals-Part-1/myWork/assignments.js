457bf7e70550c4d02d70fc213c0f61244835a798
jamesacox53 <jamesacox53@gmail.com>
2021-07-11 12:52:58 +0100

    Completed let, const and var.

--- 01-Fundamentals-Part-1/myWork/assignments.js
+++ 01-Fundamentals-Part-1/myWork/assignments.js
@@ -1,12 +1,15 @@
-let country = "United Kingdom";
-let continent = 'Europe';
+const country = "United Kingdom";
+const continent = 'Europe';
 let population = 66650000;
 
-let isIsland = true;
-let language;
+const isIsland = true;
+const language = 'English';
 
 console.log("country: " + country);
 console.log("continent: " + continent);
 console.log("population: " + population);
 console.log("isIsland: " + isIsland);
 console.log("language: " + language);
+
+// language = 'Spanish';
+// console.log("language: " + language);
