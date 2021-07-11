39dd07c54684bb02c480a370f19e774993271dd5
jamesacox53 <jamesacox53@gmail.com>
2021-07-11 12:39:25 +0100

    Completed Data Types

--- 01-Fundamentals-Part-1/myWork/assignments.js
+++ 01-Fundamentals-Part-1/myWork/assignments.js
@@ -2,6 +2,11 @@ let country = "United Kingdom";
 let continent = 'Europe';
 let population = 66650000;
 
+let isIsland = true;
+let language;
+
 console.log("country: " + country);
 console.log("continent: " + continent);
 console.log("population: " + population);
+console.log("isIsland: " + isIsland);
+console.log("language: " + language);
