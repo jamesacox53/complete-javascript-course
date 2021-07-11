3111dfa84cc487f6872e8a98facaaa88a5afcce2
jamesacox53 <jamesacox53@gmail.com>
2021-07-11 14:04:25 +0100

    Completed Basic Operators.

--- 01-Fundamentals-Part-1/myWork/assignments.js
+++ 01-Fundamentals-Part-1/myWork/assignments.js
@@ -1,9 +1,9 @@
-const country = "United Kingdom";
+const country = "The United Kingdom";
 const continent = 'Europe';
 let population = 66650000;
 
 const isIsland = true;
-const language = 'English';
+const language = 'english';
 
 console.log("country: " + country);
 console.log("continent: " + continent);
@@ -11,5 +11,20 @@ console.log("population: " + population);
 console.log("isIsland: " + isIsland);
 console.log("language: " + language);
 
-// language = 'Spanish';
-// console.log("language: " + language);
+let halfThePopulation = population / 2;
+console.log("half the population: " + halfThePopulation);
+
+population++;
+
+halfThePopulation = population / 2;
+console.log("half the population: " + halfThePopulation);
+populationOfFinland = 6000000;
+const morePeopleThanFinland = population > populationOfFinland;
+
+console.log("Does " + country + " have more people than Finland?: " + morePeopleThanFinland);
+
+const avgPopOfACountry = 33000000;
+const isPopLessThanAverage = population < avgPopOfACountry;
+
+const description = country + " is in " + continent + ", and it's " + population + " people speak " + language;
+console.log(description);
