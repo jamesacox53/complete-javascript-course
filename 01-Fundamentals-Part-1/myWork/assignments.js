0d95304c5f1481c89a186fb047f56de2f85741f8
jamesacox53 <jamesacox53@gmail.com>
2021-07-18 13:44:46 +0100

    Completed The switch Statement

--- 01-Fundamentals-Part-1/myWork/assignments.js
+++ 01-Fundamentals-Part-1/myWork/assignments.js
@@ -69,3 +69,49 @@ if (language == "english" && population < 50000000 && !isIsland) {
 } else {
     console.log(`${country} does not meet your criteria :(`);
 }
+
+const day = 'monday';
+
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+
+switch (language) {
+    case 'mandarin':
+        console.log('Most number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken language');
+        break;
+    default:
+        console.log('Great language too :D');
+        break;
+}
