daca633de5ef48cc67032453294529e61be709b1
jamesacox53 <jamesacox53@gmail.com>
2021-07-25 11:43:04 +0100

    Completed  Functions Calling Other Functions

--- 02-Fundamentals-Part-2/myWork/assignments.js
+++ 02-Fundamentals-Part-2/myWork/assignments.js
@@ -14,12 +14,12 @@ console.log(brazilString);
 function percentageOfWorld1(population) {
     return (population / 7900) * 100;
 }
-populationOfChina = 1441;
-populationOfFrance = 67;
-populationOfBrazil = 211;
-percentageOfPopChina1 = percentageOfWorld1(populationOfChina);
-percentageOfPopFrance1 = percentageOfWorld1(populationOfFrance);
-percentageOfPopBrazil1 = percentageOfWorld1(populationOfBrazil);
+const populationOfChina = 1441;
+const populationOfFrance = 67;
+const populationOfBrazil = 211;
+const percentageOfPopChina1 = percentageOfWorld1(populationOfChina);
+const percentageOfPopFrance1 = percentageOfWorld1(populationOfFrance);
+const percentageOfPopBrazil1 = percentageOfWorld1(populationOfBrazil);
 console.log(percentageOfPopChina1);
 console.log(percentageOfPopFrance1);
 console.log(percentageOfPopBrazil1);
@@ -28,17 +28,26 @@ const percentageOfWorld2 = function (population) {
     return (population / 7900) * 100;
 }
 
-percentageOfPopChina2 = percentageOfWorld2(populationOfChina);
-percentageOfPopFrance2 = percentageOfWorld2(populationOfFrance);
-percentageOfPopBrazil2 = percentageOfWorld2(populationOfBrazil);
+const percentageOfPopChina2 = percentageOfWorld2(populationOfChina);
+const percentageOfPopFrance2 = percentageOfWorld2(populationOfFrance);
+const percentageOfPopBrazil2 = percentageOfWorld2(populationOfBrazil);
 console.log(percentageOfPopChina2);
 console.log(percentageOfPopFrance2);
 console.log(percentageOfPopBrazil2);
 
 const percentageOfWorld3 = population => (population / 7900) * 100;
-percentageOfPopChina3 = percentageOfWorld3(populationOfChina);
-percentageOfPopFrance3 = percentageOfWorld3(populationOfFrance);
-percentageOfPopBrazil3 = percentageOfWorld3(populationOfBrazil);
+const percentageOfPopChina3 = percentageOfWorld3(populationOfChina);
+const percentageOfPopFrance3 = percentageOfWorld3(populationOfFrance);
+const percentageOfPopBrazil3 = percentageOfWorld3(populationOfBrazil);
 console.log(percentageOfPopChina3);
 console.log(percentageOfPopFrance3);
 console.log(percentageOfPopBrazil3);
+
+function describePopulation(country, population) {
+    const percentageOfWorld = percentageOfWorld1(population);
+    return `${country} has ${population} million people, which is about ${percentageOfWorld}% of the world.`
+}
+
+console.log(describePopulation('China', populationOfChina));
+console.log(describePopulation('France', populationOfFrance));
+console.log(describePopulation('Brazil', populationOfBrazil));
