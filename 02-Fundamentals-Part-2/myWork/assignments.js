332da02c4648e5c7622c5215af08db66293f41c9
jamesacox53 <jamesacox53@gmail.com>
2021-07-25 10:58:08 +0100

    Completed Function Declarations vs. Expressions

--- 02-Fundamentals-Part-2/myWork/assignments.js
+++ 02-Fundamentals-Part-2/myWork/assignments.js
@@ -10,3 +10,27 @@ console.log(franceString);
 
 const brazilString = describeCountry('Brazil', '211 million', 'Brasilia');
 console.log(brazilString);
+
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100;
+}
+populationOfChina = 1441;
+populationOfFrance = 67;
+populationOfBrazil = 211;
+percentageOfPopChina1 = percentageOfWorld1(populationOfChina);
+percentageOfPopFrance1 = percentageOfWorld1(populationOfFrance);
+percentageOfPopBrazil1 = percentageOfWorld1(populationOfBrazil);
+console.log(percentageOfPopChina1);
+console.log(percentageOfPopFrance1);
+console.log(percentageOfPopBrazil1);
+
+const percentageOfWorld2 = function (population) {
+    return (population / 7900) * 100;
+}
+
+percentageOfPopChina2 = percentageOfWorld2(populationOfChina);
+percentageOfPopFrance2 = percentageOfWorld2(populationOfFrance);
+percentageOfPopBrazil2 = percentageOfWorld2(populationOfBrazil);
+console.log(percentageOfPopChina2);
+console.log(percentageOfPopFrance2);
+console.log(percentageOfPopBrazil2);
