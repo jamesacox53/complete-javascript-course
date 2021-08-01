8b7adc5dc6c61fdaa54cd85401facc5374e74d28
jamesacox53 <jamesacox53@gmail.com>
2021-08-01 15:21:32 +0100

    Completed Looping Backwards and Loops in Loops

--- 02-Fundamentals-Part-2/myWork/assignments.js
+++ 02-Fundamentals-Part-2/myWork/assignments.js
@@ -123,3 +123,14 @@ for (let i = 0; i < populationsArr.length; i++) {
 }
 console.log(percentages);
 console.log(percentages2);
+
+const listOfNeighbours =
+    [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden',
+        'Russia']];
+
+for (let i = 0; i < listOfNeighbours.length; i++) {
+    for (let j = 0; j < listOfNeighbours[i].length; j++) {
+        console.log(`Neighbour: ${listOfNeighbours[i][j]}`);
+    }
+
+}
