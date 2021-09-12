fb49561e9b3644d77d85734e0f79c9d04e085a65
jamesacox53 <jamesacox53@gmail.com>
2021-09-12 16:05:54 +0100

    Started Behind the Scenes

---
+++ 08-Behind-the-Scenes/myWork/script.js
@@ -0,0 +1,92 @@
+'use strict';
+// var a = 'Jonas';
+
+// const third = function() {
+//     var d = 'Hey!';
+//     console.log(d + c + b + a);
+//     console.log("Third Function Again");
+// }
+
+// const first = function() {
+//     var b = 'Hello!';
+    
+//     const second = function() {
+//         var c = 'Hi!';
+//         console.log(c + b);
+//         third();
+//     }
+//     second();
+// }
+
+// first();
+
+// const first = function() {
+    
+//     var b = 'Hello!';
+    
+//     const third = function() {
+//         var d = 'Hey!';
+//         console.log(d + b);
+//         console.log("Third Function Again");
+//     }
+
+//     third();
+// }
+
+// first();
+
+// const firstName = 'Jonas2';
+// calcAge(1991);
+
+// function calcAge(birthYear) {
+//     const age = 2037 - birthYear;
+//     console.log(firstName);
+//     return age;
+// }
+
+// for(var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+
+
+// for(var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// var pica = "qwop";
+// console.log(pica);
+
+// var pica = "wwww";
+// console.log(pica);
+
+// console.log(pica);
+
+// function calcAge(birthYear) {
+//     const age = 2037 - birthYear;
+  
+//     function printAge() {
+//       let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//       console.log(output);
+  
+//       if (birthYear >= 1981 && birthYear <= 1996) {
+//         firstName = 'Steven';
+//         var millenial = true;
+//         const str = `Oh, and you're a millenial, ${firstName}`;
+//         console.log(str);
+//       }
+//       // console.log(str);
+//       console.log(millenial);
+//       console.log(firstName);
+//     }
+//     printAge();
+  
+//     return age;
+//   }
+  
+//   let firstName = 'Jonas';
+//   calcAge(1991);
+
+// var x = 1;
+// var y = 14;
+  
