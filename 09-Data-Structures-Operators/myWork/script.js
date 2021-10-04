d229d9c7644a73bc95e6953297915e7283066daf
jamesacox53 <jamesacox53@gmail.com>
2021-10-04 14:44:03 +0100

    Completed Maps: Iteration

--- 09-Data-Structures-Operators/myWork/script.js
+++ 09-Data-Structures-Operators/myWork/script.js
@@ -59,3 +59,25 @@ const mapTest = new Map();
 mapTest.set(document.querySelector('h1'), 'This is a test.');
 
 console.log(mapTest.get(document.querySelector('h1')));
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
