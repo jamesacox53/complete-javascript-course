f598f64cce1047bf9a052e780cd03a86bb1bc6e9
jamesacox53 <jamesacox53@gmail.com>
2021-10-17 16:48:06 +0100

    Completed PROJECT: "Bankist" Website

---
+++ 13-Advanced-DOM-Bankist/myWork/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (event) {
+  event.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+// PROJECT: "Bankist" Website
+
+btnsOpenModal.forEach(button => button.addEventListener('click', openModal));
